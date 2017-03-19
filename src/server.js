c49797ccdbdc97011a3e2860db2b18a1e235a4dd
Jeustace <jeustace@newdistrict.ca>
2017-03-18 20:17:40 -0700

    Implemented authentication route.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import Hapi from 'hapi';
 import {jwtKey} from '../credentials';
 import Knex from './knex';
+import jwt from 'jsonwebtoken';
 
 const server = new Hapi.Server();
 
@@ -22,6 +23,7 @@ server.register(require('hapi-auth-jwt'), err => {
     });
 });
 
+//GET route for birds
 server.route({
 
     path: '/birds',
@@ -42,6 +44,47 @@ server.route({
                 dataCount: results.length,
                 data: results,
             });
+        }).catch(err => {
+            reply(`server-side error: ${err}`);
+            errorHandler('server-side error:', err);
+        });
+    }
+});
+
+//POST route for authentication
+server.route({
+    path: '/auth',
+    method: 'POST',
+    handler: (request, reply) => {
+        const {username, password} = request.payload;
+        Knex('users')
+            .where({username})
+            .select('guid', 'password').then(([user]) => {
+            if (!user) {
+                reply({
+                    error: true,
+                    errMessage: 'the specified user was not found',
+                });
+                // We don't want to wrap everything else in an `else` block, better to just return the control.
+                return;
+            }
+            //TODO: Use salted-hashing algorithm to compare.
+            if (user.password === password) {
+                const token = jwt.sign({
+                    // You can have anything you want here. ANYTHING. This decoded token will be passed onto a request handler.
+                    username,
+                    scope: user.guid,
+                }, 'vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy', {
+                    algorithm: 'HS256',
+                    expiresIn: '1h',
+                });
+                reply({
+                    token,
+                    scope: user.guid,
+                });
+            } else {
+                reply('incorrect password');
+            }
         }).catch(err => {
             reply(`server-side error: ${err}`);
             errorHandler('server-side error:', err);
